1566ef486e6695675f1fd7b2aed97a5137c2611b
Carlos Eduardo Papacidero <carlos.papacidero@venturus.org.br>
2016-05-22 18:42:10 -0300

    New Menu Style

--- es6/script.js
+++ es6/script.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
 
+    var actualSLide = 1;
 
-    $('.circle').on('click', (e) => {
-
-        let targetElement = $('.slide-' + e.target.dataset.index);
-        console.log(targetElement);
-
+    let scrollToSlide = (slide) => {
+        $('.selected').removeClass('selected');
+        let targetElement = $('.slide-' + slide);
+        $('.circle[data-index="'+slide+'"]').addClass('selected')
+        actualSLide = slide;
         $('html, body').animate({
             scrollTop: targetElement.offset().top
         }, 500);
-
+    }
+    
+    $('body').on('keydown', (e) => {
+        if (actualSLide > 1 && e.keyCode == 38) {
+            scrollToSlide(actualSLide-1)
+        }
+        if (actualSLide < 8 && e.keyCode == 40) {
+            scrollToSlide(actualSLide+1)
+        }
+        
     })
+    
+
+    $('.circle').on('click', (e) => {
+        let targetSlide = e.target.dataset.index;
+        scrollToSlide(targetSlide)
+    })
+
+    
 
 });
--- js/script.js
+++ js/script.js
@@ -2,13 +2,40 @@
 
 $(document).ready(function () {
 
-    $('.circle').on('click', function (e) {
+    var actualSLide = 1;
 
-        var targetElement = $('.slide-' + e.target.dataset.index);
-        console.log(targetElement);
-
+    var scrollToSlide = function scrollToSlide(slide) {
+        $('.selected').removeClass('selected');
+        var targetElement = $('.slide-' + slide);
+        $('.circle[data-index="' + slide + '"]').addClass('selected');
+        actualSLide = slide;
         $('html, body').animate({
             scrollTop: targetElement.offset().top
-        }, 500);
+        }, 500, function(){
+            console.log('After Animation');
+        });
+    };
+    
+    scrollToSlide(1);
+
+    // $('body').on('keydown', function (e) {
+    //     if (actualSLide > 1 && e.keyCode == 38) {
+    //         scrollToSlide(actualSLide - 1);
+    //     }
+    //     if (actualSLide < 8 && e.keyCode == 40) {
+    //         scrollToSlide(actualSLide + 1);
+    //     }
+    // });
+    
+    
+    $('.scroll').on('click', (e)=> {
+        if (actualSLide < 8) {
+            scrollToSlide(actualSLide + 1);
+        }
+    })
+
+    $('.circle').on('click', (e) => {
+        var targetSlide = e.target.dataset.index;
+        scrollToSlide(targetSlide);
     });
 });
